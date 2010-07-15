d973f0a6329b413eda6a5e3b0de61fd7b503cf7d
Mat Schaffer <mat.schaffer@gmail.com>
2010-07-15 07:54:44 -0400

    Include rspec mocha adapter to get mocks reset correctly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $: << File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'ec2manage'
 require 'mocha'
+require 'spec/adapters/mock_frameworks/mocha'
 
 # This is Rubinius-specific
 require 'debugger'
